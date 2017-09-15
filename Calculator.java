3c54fe43cae9d34db53112dbe47da5e44c106a31
Hyesung Ko <kohs0429@gmail.com>
2017-09-14 18:42:28 -0700

    add some function names

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,29 @@
 class Calculator {
+
+    public void add(){
+        
+    }
+
+    public void sub(){
+
+    }
+
+    public void multiply(){
+
+    }
+
+    public void divide(){
+
+    }
+
+    public void sqrt(){
+
+    }
+
+    public void power(){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("HelloWorld");
     }
